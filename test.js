125c8d12ebcbdfdb152d55d9801574eba7bd8f56
islam030804 <globus.user95@gmail.com>
2022-11-07 18:52:17 +0600

    второй этап

--- test.js
+++ test.js
@@ -1,21 +1,40 @@
 const button = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-const colors = [
-  " #FF5733 ",
-  " #FFFF33 ",
-  " #61FF33 ",
-  " #33FFE3 ",
-  " #3355FF ",
-  " #FF3386 ",
+const hex = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
 ];
 
 button.addEventListener("click", () => {
-  let hexColor = colors[getRandomColor()];
+  let hexColor = generateHex();
   document.body.style.backgroundColor = hexColor;
   color.textContent = hexColor;
 });
 
+function generateHex() {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomColor()];
+  }
+
+  return hexColor;
+}
+
 function getRandomColor() {
-  return Math.floor(Math.random() * colors.length);
+  return Math.floor(Math.random() * hex.length);
 }
